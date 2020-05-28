fcf72ef197c217b01b1962fbce58f42babc9ebb2
Zachariah Chow <zachariahchow@gmail.com>
2020-05-28 18:51:57 +0800

    refactor: Create App component

---
+++ src/client/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import tweets from 'tweets';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                tweets
+            </div>
+        );
+    }
+}
+
+export default App
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-import tweets from 'tweets'
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        tweets
-      </div>
-    );
-  }
-}
+import tweets from 'tweets';
 
 const element = document.getElementById('app');
 
-ReactDOM.render(<App />, element );//
-
-console.log("tweet react");
+ReactDOM.render(<App />, element); //
