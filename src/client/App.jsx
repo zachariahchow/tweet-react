045003a56b4cfce34c11c2d41c9de2652ba32275
Zachariah Chow <zachariahchow@gmail.com>
2020-05-28 19:19:45 +0800

    feat: Update tweet component (render tweet text)

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 
 import Tweet from './Tweet/Tweet';
 
-import tweets from 'tweets';
+import { tweets } from 'tweets';
 
 class App extends React.Component {
+
+
     render() {
+
+        const allTweets = tweets.map(tweet => {
+            return (
+                <Tweet text={tweet.text}/>
+            )
+        })
+
         return (
             <div>
-                <Tweet />
+                {allTweets}
             </div>
         );
     }
 }
+
+console.log(tweets[0].text);
 
 export default App
--- src/client/Tweet/Tweet.jsx
+++ src/client/Tweet/Tweet.jsx
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import tweets from 'tweets';
 
 
-const tweet = () => {
+const tweet = (props) => {
+
     return (
         <div className="tweet-text__container">
-            <p className="tweet-text__content">Tweet!</p>
+            <p className="tweet-text__content">{props.text}</p>
         </div>
     );
 }
