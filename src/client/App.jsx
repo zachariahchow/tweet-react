fc89e9096b6bf9ad7064695159ed22ca10095b89
Zachariah Chow <zachariahchow@gmail.com>
2020-05-28 19:02:16 +0800

    feat: Create tweet component scaffold

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Tweet from './Tweet/Tweet';
+
 import tweets from 'tweets';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                tweets
+                <Tweet />
             </div>
         );
     }
---
+++ src/client/Tweet/Tweet.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import tweets from 'tweets';
+
+
+const tweet = () => {
+    return (
+        <div className="tweet-text__container">
+            <p className="tweet-text__content">Tweet!</p>
+        </div>
+    );
+}
+
+
+export default tweet;
