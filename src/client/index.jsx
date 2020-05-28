417c1dd8d8fc6376b1f06ffe266942eb8b698e14
Zachariah Chow <zachariahchow@gmail.com>
2020-05-28 20:20:07 +0800

    fix: Remove unnecessary import

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import tweets from 'tweets';
-
 const element = document.getElementById('app');
 
 ReactDOM.render(<App />, element); //
